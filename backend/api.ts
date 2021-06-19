6c1b777da5767cef0414847ac0ce1ae60511bb23
carlosmg12 <carlos.mendez.g01@mail.pucv.cl>
2021-06-19 01:35:16 -0400

    algunos metodos de consulta mysql

--- backend/api.ts
+++ backend/api.ts
@@ -1,9 +1,11 @@
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
+const bodyParser = require("body-parser");
 
-const servidor = "127.0.0.1";
-const port = 3000;
+const configServidor={
+    servidor    : "127.0.0.1",
+    port        : 3000}
 
 let connection = mysql.createConnection({
     host        : "127.0.0.1",
@@ -21,12 +23,21 @@ connection.connect((error:any)=>{
     console.log("conexion realizada con exito");
 })
 
+app.use(bodyParser.urlencoded({ extended:false }));
+
 app.get("/",(req:any,res:any)=>{
     res.send("hola mundo");
 });
 
 app.get("/tickets",(req:any,res:any)=>{
-    connection.query("select * from tickets",(req1:any,res1:any)=>{
+    connection.query("SELECT * FROM tickets",(req1:any,res1:any)=>{
+        //res.send(res1);
+        res.status(200).send(res1);
+    });
+});
+
+app.get("/usuarios",(req:any,res:any)=>{
+    connection.query("SELECT * FROM usuarios",(req1:any,res1:any)=>{
         //res.send(res1);
         res.status(200).send(res1);
     });
@@ -34,13 +45,41 @@ app.get("/tickets",(req:any,res:any)=>{
 
 app.get("/tickets/:id",(req:any,res:any)=>{
     let id=req.params.id;
-    connection.query("select * from tickets where idTicket = ?",id,(req1:any,res1:any)=>{
+    connection.query("SELECT * FROM tickets WHERE idTicket = ?",id,(req1:any,res1:any)=>{
         //res.send(res1);
         res.status(200).send(res1);
     });
 });
+//[estado,prioridad,idUsuario,asunto,descripcion,categoria]
+app.post('/crearusuario',(req:any,res:any)=>{
+    let nombre=req.body.nombre;
+    let apellido=req.body.apellido;
+    let rut=req.body.rut;
+    let direc=req.body.direc;
+    let region=req.body.region;
+    let comuna=req.body.comuna;
+    let correo=req.body.correo;
+    let contrasena=req.body.contrasena;
+    connection.query("INSERT INTO usuarios(nombre,apellido,rut,direccion,region,comuna,correo_electrico,contrasena)VALUES('"+nombre+"','"+apellido+"','"+rut+"','"+direc+"','"+region+"','"+comuna+"','"+correo+"','"+contrasena+"')",(req1:any,res1:any)=>{
+        res.status(201).send("usuario creado de pana mi rey su valorant");
+    })
+});
 
+app.put('/editarusuario/:id',(req:any,res:any)=>{
+    let id=req.params.id;
+    let nombre=req.body.nombre;
+    let apellido=req.body.apellido;
+    let rut=req.body.rut;
+    let direc=req.body.direc;
+    let region=req.body.region;
+    let comuna=req.body.comuna;
+    let correo=req.body.correo;
+    let contrasena=req.body.contrasena;
+    connection.query("UPDATE usuarios SET nombre=?,apellido=?,rut=?,direccion=?,region=?,comuna=?,correo_electrico=?,contrasena=? WHERE idUsuario=?",[nombre,apellido,rut,direc,region,comuna,correo,contrasena,id],(req1:any,res1:any)=>{
+        res.status(200).send("usuario actualizado");
+    })
+});
 
-app.listen(port,()=>{
-    console.log(`${port}`);
+app.listen(configServidor,()=>{
+    console.log(`${configServidor.port}`);
 });
--- backend/js/api.js
+++ backend/js/api.js
@@ -2,8 +2,11 @@
 var express = require("express");
 var mysql = require("mysql");
 var app = express();
-var servidor = "127.0.0.1";
-var port = 3000;
+var bodyParser = require("body-parser");
+var configServidor = {
+    servidor: "127.0.0.1",
+    port: 3000
+};
 var connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
@@ -18,22 +21,57 @@ connection.connect(function (error) {
     }
     console.log("conexion realizada con exito");
 });
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get("/", function (req, res) {
     res.send("hola mundo");
 });
 app.get("/tickets", function (req, res) {
-    connection.query("select * from tickets", function (req1, res1) {
+    connection.query("SELECT * FROM tickets", function (req1, res1) {
+        //res.send(res1);
+        res.status(200).send(res1);
+    });
+});
+app.get("/usuarios", function (req, res) {
+    connection.query("SELECT * FROM usuarios", function (req1, res1) {
         //res.send(res1);
         res.status(200).send(res1);
     });
 });
 app.get("/tickets/:id", function (req, res) {
     var id = req.params.id;
-    connection.query("select * from tickets where idTicket = ?", id, function (req1, res1) {
+    connection.query("SELECT * FROM tickets WHERE idTicket = ?", id, function (req1, res1) {
         //res.send(res1);
         res.status(200).send(res1);
     });
 });
-app.listen(port, function () {
-    console.log("" + port);
+//[estado,prioridad,idUsuario,asunto,descripcion,categoria]
+app.post('/crearusuario', function (req, res) {
+    var nombre = req.body.nombre;
+    var apellido = req.body.apellido;
+    var rut = req.body.rut;
+    var direc = req.body.direc;
+    var region = req.body.region;
+    var comuna = req.body.comuna;
+    var correo = req.body.correo;
+    var contrasena = req.body.contrasena;
+    connection.query("INSERT INTO usuarios(nombre,apellido,rut,direccion,region,comuna,correo_electrico,contrasena)VALUES('" + nombre + "','" + apellido + "','" + rut + "','" + direc + "','" + region + "','" + comuna + "','" + correo + "','" + contrasena + "')", function (req1, res1) {
+        res.status(201).send("usuario creado de pana mi rey su valorant");
+    });
+});
+app.put('/editarusuario/:id', function (req, res) {
+    var id = req.params.id;
+    var nombre = req.body.nombre;
+    var apellido = req.body.apellido;
+    var rut = req.body.rut;
+    var direc = req.body.direc;
+    var region = req.body.region;
+    var comuna = req.body.comuna;
+    var correo = req.body.correo;
+    var contrasena = req.body.contrasena;
+    connection.query("UPDATE usuarios SET nombre=?,apellido=?,rut=?,direccion=?,region=?,comuna=?,correo_electrico=?,contrasena=? WHERE idUsuario=?", [nombre, apellido, rut, direc, region, comuna, correo, contrasena, id], function (req1, res1) {
+        res.status(200).send("usuario actualizado");
+    });
+});
+app.listen(configServidor, function () {
+    console.log("" + configServidor.port);
 });
